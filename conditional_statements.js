613f66c8d6e7ecf359d4f4f2255d385d103da0ed
ummeabiha <rizvi4400977@cloud.neduet.edu.pk>
2024-02-05 02:28:39 +0500

    conditional statements

---
+++ conditional_statements.js
@@ -0,0 +1,15 @@
+let mode = "dark";
+let color;
+
+if (mode == "dark") {
+  color = "black";
+} else if (mode == "light") {
+  color = "blue";
+} else if (mode == "white") {
+  color = "pink";
+} else {
+  color = "white";
+}
+console.log(color);
+
+if (mode == "dark") console.log(color);
