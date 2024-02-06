a0ce51958fafe51f600c505ab6c9ae8dab29e902
ummeabiha <rizvi4400977@cloud.neduet.edu.pk>
2024-02-06 19:27:00 +0500

    DOM

--- DOM.js
+++ DOM.js
@@ -30,9 +30,51 @@ console.log(window.document);
 console.log(document.body);
 console.log(document.head);
 
-document.body.style.background ='red';
-document.body.childNodes[3].innerText= 'abcd';
+document.body.style.background = "red";
+document.body.childNodes[3].innerText = "abcd";
 
 //DOM MANIPULATION
-document.getElementById('heading');
-document.getElementsByClassName('heading2');
+
+//SELECTION WITH ID
+console.dir(document.getElementById("heading"));
+//when the id element is not present in html, & we try to access it using JS, the output will be shown as "null"
+console.dir(document.getElementById("headings"));
+
+//************************************************************** */
+//**************ACCESSING DOM ELEMENTS***************/
+
+//SELECTION WITH CLASS NAME
+let heading2 = document.getElementsByClassName("heading2");
+console.dir(heading2);
+console.log(heading2);
+//when the class element is not present in html, & we try to access it using JS, the output will be shown as empty html collection i.e. HTMLCollection[]
+console.log(document.getElementsByClassName("headings"));
+
+/************************************************************** */
+//SELECTION WITH TAG
+console.log(document.getElementsByTagName("p"));
+
+/************************************************************** */
+//QUERY SELECTOR
+console.dir(document.querySelector("p")); //tag sy matching 1st element will be returned
+console.dir(document.querySelectorAll("p")); //tag sy matching all elements i.e. Node List will be returned
+
+//************************************************************** */
+//**************PROPERTIES OF DOM ELEMENTS***************/
+
+let divEle = document.querySelector("div");
+console.log(divEle);
+
+// 1- TAG NAME
+console.log(divEle.tagName);
+
+// 2- INNER TEXT (text content of element along with its children)
+console.log(divEle.innerText);
+// divEle.innerText= "none";
+
+// 3- INNER HTML (plain text content or HTML content of element)
+console.log(divEle.innerHTML);
+// divEle.innerHTML= "<div>inner div</div>";
+
+// 4- TEXT CONTENT (returns text content even for hidden elements)
+console.log(divEle.textContent);
