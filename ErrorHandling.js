2ccfbb1d7239d70b20336f5945f4e58c047d4231
ummeabiha <rizvi4400977@cloud.neduet.edu.pk>
2024-02-14 01:32:26 +0500

    added finally

--- ErrorHandling.js
+++ ErrorHandling.js
@@ -8,5 +8,25 @@ catch(err){
     console.log(err);
     console.log(err.message);
 }
+finally{
+    console.log("I will run in any condition (even when try and catch both contains the error or not)");
+    // Close the file/ connection
+    // Exit the loop
+    // Write to the log file
+}
 
 console.log(`a=${a}\nb=${b}\na+b=${a + b}`);
+
+const fun=()=>{
+    try{
+        let a=0; 
+        console.log("Program ran successfully");
+        return;
+    }
+    catch(e){
+        console.log("Error");
+    }
+    finally{
+        console.log("I will run even when the function is returned from the try block.");
+    }
+}
